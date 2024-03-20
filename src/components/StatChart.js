672ccf270be0cedbcaf1a6cd97d5002c453ff6a2
Alperen Ustaömer <alperen.ustaomer61@gmail.com>
2024-03-21 00:29:27 +0300

    Fix tooltip of chart

--- src/components/StatChart.js
+++ src/components/StatChart.js
@@ -13,14 +13,27 @@ export const StatChart = ({data, name}) => {
         })))
     }, [data]);
 
+    const CustomTooltip = ({active, payload, label}) => {
+        if (active && payload && payload.length) {
+            const edge = payload[0].payload;
+            return (
+                <div style={{padding: '1rem', background: '#C08D55', color: 'white'}}>
+                    <h4>{`${label}: ${edge.base_stat}`}</h4>
+                </div>
+            );
+        }
+        return null;
+    };
+
     return (
         <ResponsiveContainer width="100%" height="100%">
             <RadarChart cx="50%" cy="50%" outerRadius="80%" data={statData}>
                 <PolarGrid/>
-                <Tooltip />
+                <Tooltip content={<CustomTooltip/>}/>
                 <PolarAngleAxis dataKey="stat"/>
                 <PolarRadiusAxis domain={[0, 140]}/>
-                <Radar name={capitilazeFirstLetter(name)} dataKey="base_stat" stroke={systemColors.yellow} fill={systemColors.yellow} fillOpacity={0.6} isAnimationActive={false}/>
+                <Radar name={capitilazeFirstLetter(name)} dataKey="base_stat" stroke={systemColors.yellow}
+                       fill={systemColors.yellow} fillOpacity={0.6} isAnimationActive={false}/>
             </RadarChart>
         </ResponsiveContainer>
     );
